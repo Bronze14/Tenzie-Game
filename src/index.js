317462a4191cb6bf8351b12d0f4ae626e490d011
Bronze14 <marcindadek@gmail.com>
2023-02-18 19:12:48 +0100

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from "react"
+import ReactDOM from "react-dom"
+import App from "./App"
+import "./style.css"
+
+function Index(){
+    return (
+        <App />
+    )
+}
+ReactDOM.render(<Index />,document.getElementById("root"))
